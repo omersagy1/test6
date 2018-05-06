d3cacf69db29cf6910377e12a2c03efcddaa695b
omersagy <omersagyemails@gmail.com>
2018-05-06 14:30:41 +0200

    border highlights on active/inactive

--- src/game/render/meter.js
+++ src/game/render/meter.js
@@ -6,16 +6,24 @@ const BAR_COLOR = '#909090';
 const BG_COLOR = 'dimgray';
 const LABEL_COLOR = 'white';
 
-const BORDER_COLOR = 'yellow';
+const ACTIVE_BORDER_COLOR = 'yellow';
+const INACTIVE_BORDER_COLOR = 'darkgray';
 const BORDER_WIDTH = '1px';
 
 const Meter = ({proportion, label, callback, bar_color}) => {
+  let border_color;
+  if (proportion == 0) {
+    border_color = ACTIVE_BORDER_COLOR;
+  } else {
+    border_color = INACTIVE_BORDER_COLOR;
+  }
+
   const outer_style = {
     width: WIDTH,
     height: HEIGHT,
     background: BG_COLOR,
     color: LABEL_COLOR,
-    borderColor: BORDER_COLOR,
+    borderColor: border_color,
     borderWidth: '1px',
     borderStyle: 'solid',
     fontSize: 16
